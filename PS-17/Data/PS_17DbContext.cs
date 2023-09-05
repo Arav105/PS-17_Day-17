ad572d1d87550b79f87676ae70a7ef3dd1facbc1
Arav105 <chachuarav@gmail.com>
2023-09-05 14:54:48 +0530

    PS-17

---
+++ PS-17/Data/PS_17DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PS_17.Model;
+
+namespace PS_17.Data
+{
+    public class PS_17DbContext : DbContext
+    {
+        public PS_17DbContext (DbContextOptions<PS_17DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PS_17.Model.Movies> Movies { get; set; } = default!;
+    }
+}
---
+++ PS-17/Model/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PS_17.Model
+{
+    [Table("Movie")]
+    public class Movies
+    { 
+
+
+
+            [Key]
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Director { get; set; }
+            public DateTime ReleaseDate { get; set; }
+            public string Genre { get; set; }
+            public double Rating { get; set; }
+        
+    }
+}
